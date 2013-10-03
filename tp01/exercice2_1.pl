9a2319c7bec08e47f724f0eb4b47339009787b51
jehu <antoine.hars@etu.utc.fr>
2013-10-04 00:38:15 +0200

    exo2

---
+++ tp01/exercice2_1.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+# UV : LO17 - TP01
+# Préparation du Corpus
+# Auteurs : Dany Ferreira - Antoine Hars
+# Fichier : exercice2_1.pl
+
+$fichier = $ARGV[0];
+
+open(FICHIN, "LCI_EXTRACT_1/$fichier") or die "Cannot open fichier: $!";
+chdir("LCI_EXTRACT_2/");
+open(FICHOUT, ">$fichier") or die "Cannot open fichier: $!";
+
+while($a = <FICHIN>) {
+
+	# Mise en forme du doc sur une seule ligne.
+	while ($a =~ s/(\r|\n|\lf)//g) 
+	{
+		#print "$1\n";
+	}	
+	#print "$a";
+	print FICHOUT $a;
+}
+
+close(FICHOUT);
+close(FICHIN);
+
---
+++ tp01/exercice2_2.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+# UV : LO17 - TP01
+# Préparation du Corpus
+# Auteurs : Dany Ferreira - Antoine Hars
+# Fichier : exercice2_2.pl
+
+mkdir('LCI_EXTRACT_2',0755) || die ("Err. Cr. répertoire \n");
+
+@files = `ls LCI_EXTRACT_1/`;
+$nbFiles = 0;
+
+foreach $elt (@files) {
+
+	print "$elt \n";
+	$nbFiles++;
+	$commande = "./exercice2_1.pl $elt";
+	system($commande) == 0 or die "Erreur de la commande : $commande\n";
+}
+
+print "Nombre de fichiers traités : $nbFiles\n";
+
