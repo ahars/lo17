6d8e803e3acd363114a8db5132007747aad3622b
jehu <antoine.hars@etu.utc.fr>
2013-10-05 16:16:27 +0200

    correction erreurs

--- tp01/exercice4_3.pl
+++ tp01/exercice4_3.pl
@@ -11,46 +11,55 @@ open(FICHIN, "LCI_EXTRACT_3/$fichier") or die "Cannot open fichier: $!";
 chdir("LCI_EXTRACT_4/");
 open(FICHOUT, ">>$fichier") or die "Cannot open fichier: $!";
 
-print FICHOUT "<FOCUS>";
-print FICHOUT "\n";
+print FICHOUT "\t\t<FOCUS>\n";
 
 # Traitement sur le FOCUS.
 while($a = <FICHIN>) {
 
 	if($a =~/^<FOCUS>/) {
 
-		$a =~/\<a[^>]*hre=(.*?)\sclass="S48">/;
-		print FICHOUT "\t<urlArticle>$1</urlArticle>\n";
+		if ($a =~/\<a[^>]*href=(.*?)\sclass="S48">/) {
+			print FICHOUT "\t\t\t<urlArticle>$1</urlArticle>\n";
+		} else {
+			print FICHOUT "\t\t\t<urlArticle>PAS D'INFORMATIONS</urlArticle>\n";
+		}
 
-		$a =~/">(.*?)<\/a>/;
-		print FICHOUT "\t<titreArticle>$1</titreArticle>\n";
+		if ($a =~/">(.*?)<\/a>/) {
+			print FICHOUT "\t\t\t<titreArticle>$1</titreArticle>\n";
+		} else {
+			print FICHOUT "\t\t\t<titreArticle>PAS D'INFORMATIONS</titreArticle>\n";
+		}
 
 		# On suppose que la date de l'article est la date de la page
 		$fichier =~ /(\d\d\d\d)-(\d\d)-(\d\d)/;
-		print FICHOUT "\t<dateArticle>$3/$2/$1</dateArticle>\n";
-		
-		$a =~/\<img src=(.*?)\sborder/;
-		@words = split(/\s+/, $1);
+		print FICHOUT "\t\t\t<dateArticle>$3/$2/$1</dateArticle>\n";
 
-		foreach $elt (@words) {
-
-			if($elt=~/.jpg/) {
-
-				$elt =~ s/src=// ;
-				print FICHOUT "\t<urlImage>$elt</urlImage>\n";
-			}
+		if ($a =~/<img\ssrc="(.*?)"/) {
+			print FICHOUT "\t\t\t<urlImage>$1</urlImage>\n";
+		} else {
+			print FICHOUT "\t\t\t<urlImage>PAS D'INFORMATIONS</urlImage>\n";
 		}
-		$a =~/class="S48">(.*?)<\/a>/;
-		print FICHOUT "\t<resumeArticle>$1</resumeArticle>\n";
 
-		$a =~/<a hre="mailto:(.*?)"/;
-		print FICHOUT "\t<mailto>$1</mailto>\n";
+		if ($a =~/class="S48">(.*?)<\/a>/) {
+			print FICHOUT "\t\t\t<resumeArticle>$1</resumeArticle>\n";
+		} else {
+			print FICHOUT "\t\t\t<resumeArticle>PAS D'INFORMATIONS</resumeArticle>\n";
+		}
 
-		$a =~/class="S14">(.*?)<\/a>/;
-		print FICHOUT "\t<auteur>$1</auteur>";
+		if ($a =~/<a href="mailto:(.*?)"/) {
+			print FICHOUT "\t\t\t<mailto>$1</mailto>\n";
+		} else {
+			print FICHOUT "\t\t\t<mailto>PAS D'INFORMATIONS</mailto>\n";
+		}
+
+		if ($a =~/class="S14">(.*?)<\/a>/) {
+			print FICHOUT "\t\t\t<auteur>$1</auteur>\n";
+		} else {
+			print FICHOUT "\t\t\t<auteur>PAS D'INFORMATIONS</auteur>\n";
+		}
 	}
 }
-print FICHOUT "\n</FOCUS>\n";
+print FICHOUT "\t\t</FOCUS>\n";
 
 close(FICHOUT);
 close(FICHIN);
