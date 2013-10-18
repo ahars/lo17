496b857885b83d636add8952b298825929e62176
jehu <antoine.hars@etu.utc.fr>
2013-10-18 23:40:34 +0200

    Ajout test exo2

--- tp01/exercice2_2.pl
+++ tp01/exercice2_2.pl
@@ -8,15 +8,37 @@
 mkdir('LCI_EXTRACT_2',0755) || die ("Err. Cr. répertoire\n");
 
 @files = `ls LCI_EXTRACT_1/`;
+
+$testNbFiles = `find LCI_EXTRACT_1/ -type f | wc -l`;
 $nbFiles = 0;
+
+$nbLigneFiles = 0;
 
 foreach $elt (@files) {
 
-	print "$elt";
+	print "Traitement de : $elt";
 	$nbFiles++;
 	$commande = "./exercice2_1.pl $elt";
 	system($commande) == 0 or die "Erreur de la commande : $commande\n";
 }
 
-print "Nombre de fichiers traités : $nbFiles\n";
+print "\n";
+
+# Test du nombre de lignes dans les fichiers une fois traités.
+@files = `ls LCI_EXTRACT_2/`;
+foreach $elt (@files) {
+
+	$commande = "wc -l LCI_EXTRACT_2/$elt";
+	system($commande) == 0 or die "Erreur de la commande : $commande\n";
+
+	# Pour une raison inconnue, wc -l sur les fichiers renvoie un nombre de lignes = 0 alors que nous avons une ligne...
+	if ($1 < 2) {
+		$nbLigneFiles++;
+	}
+}
+
+print "\n====================TESTS====================\n";
+print "\nNombre de fichiers traités : $nbFiles / $testNbFiles";
+print "\nNombre de fichiers finaux ne contenant qu'une ligne : $nbLigneFiles / $testNbFiles";
+print "\n====================TESTS====================\n\n";
 
